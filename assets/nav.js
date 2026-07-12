91b23d868997eb07867c6532109545b3fb188141
70Xmedia <70xmedia@pm.me>
2026-07-12 17:53:46 +0700

    Update nav.js

--- assets/nav.js
+++ assets/nav.js
@@ -7,3 +7,29 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+// Toggle menu
+const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('.nav-menu');
+
+navToggle.addEventListener('click', function() {
+  this.classList.toggle('active');
+  navMenu.classList.toggle('open');
+});
+
+// Close menu with X button
+const closeMenu = document.querySelector('.nav-menu .close-menu');
+if (closeMenu) {
+  closeMenu.addEventListener('click', function() {
+    navToggle.classList.remove('active');
+    navMenu.classList.remove('open');
+  });
+}
+
+// Close menu when clicking outside
+document.addEventListener('click', function(e) {
+  if (!navMenu.contains(e.target) && !navToggle.contains(e.target)) {
+    navToggle.classList.remove('active');
+    navMenu.classList.remove('open');
+  }
+});
